042ad43428102903be4ddc44d778b8603e5635b7
matthewmungall96 <matthewmungall96@gmail.com>
2020-03-30 15:38:33 +0100

    Push

--- Food_Fight/src/scenes/LoadScene.js
+++ Food_Fight/src/scenes/LoadScene.js
@@ -11,8 +11,9 @@ var time = 0;
 loadScene.preload = function () {
     this.load.image("title_bg", "./dist/assets/images/background.jpg");
     this.load.image("logo", "./dist/assets/images/foodFight.png");
-    this.load.image("singleplayer_button", "./dist/assets/images/single.png")
-    this.load.image("multiplayer_button", "./dist/assets/images/multi.png")
+    this.load.image("singleplayer_button", "./dist/assets/images/G.U.I/single.png");
+    this.load.image("multiplayer_button", "./dist/assets/images/G.U.I./multi.png");
+    this.load.image("narrative_button", "./dist/assets/images/G.U.I./narrative.png");
     this.load.image("returntomenu", "./dist/assets/images/returntomenu.png");
 
     //Loading Game Music Files
