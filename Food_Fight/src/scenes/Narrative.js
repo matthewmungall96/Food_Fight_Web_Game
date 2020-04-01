9737f666583ba15ac7c8ef06eba3f81e7ac3559f
Robbie1888 <robbiemack100@gmail.com>
2020-04-01 15:54:37 +0100

    narrative scene setup

--- Food_Fight/src/scenes/Narrative.js
+++ Food_Fight/src/scenes/Narrative.js
@@ -3,14 +3,8 @@ let narrativeScene = new Phaser.Scene('Narrative');
 narrativeScene.preload = function(){
    
 }
-var buttonPress;
 narrativeScene.create = function(){
-    //
-    if (game.input.mouse.locked){
-        game.input.mouse.releasePointerLock();
-    }
     
-    //
     this.add.image(this.game.renderer.width /2, this.game.renderer.height / 2, "narBground").setDepth(2).setScale(1);
     
 }
