164e9399c7fd796a017f54a1d9885b58a808b42c
matthewmungall96 <matthewmungall96@gmail.com>
2020-04-01 15:36:37 +0100

    Fixed Narrative

--- Food_Fight/src/scenes/Narrative.js
+++ Food_Fight/src/scenes/Narrative.js
@@ -1,10 +1,13 @@
 let narrativeScene = new Phaser.Scene('Narrative');
 
 narrativeScene.preload = function(){
+    
 }
 
 narrativeScene.create = function(){
+
 }
 
 narrativeScene.update = function(){
+
 }
