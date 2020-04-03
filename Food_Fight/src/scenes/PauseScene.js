3a5a70a0af8f4a55a0ba1f20b392a9ef0dcce402
matthewmungall96 <matthewmungall96@gmail.com>
2020-04-03 10:57:57 +0100

    Update PauseScene.js

--- Food_Fight/src/scenes/PauseScene.js
+++ Food_Fight/src/scenes/PauseScene.js
@@ -1,17 +1,48 @@
+//Creation of the scene object
 let pauseScene = new Phaser.Scene('Pause');
 
+/**
+* Will import all the items used through the game, all sprites, musics and tile maps
+*/
 pauseScene.preload = function () {
 
 }
 
-
+/**
+ * Pause Menu Create Function
+ * Creates the assets for the Pause Menu scene, which have been established in the LoadScene.js file 
+ */
 pauseScene.create = function () {
+    //Disables the mouse lock established in the Single Player or Multiplayer scenes
     if (game.input.mouse.locked) {
         game.input.mouse.releasePointerLock();
     }
-
+    /**
+     * Creates the individual buttons for accessing the different scenes, which are;
+     * 1. return2Button 
+     * 2. return2Menu
+    */
     return2Button = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 1.1, 'return').setDisplaySize(300, 80).setDepth(1).setScale(0.3);
+    return2Menu = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2, 'singleplayer_button').setDisplaySize(600, 160).setDepth(1).setScale(0.3);
+
+    /**
+     * .SetInteractive();
+     * Sets the individual buttons as being interactable, allowing them to be interacted with via the mouse
+    */
     return2Button.setInteractive();
+    return2Menu.setInteractive();
+
+    /**
+     * .on Functionality 
+     * Sets up the method of accessing the function of the button, via pressing the left-mouse button. 
+     */
+
+    /**
+     * return2Button.on
+     * When return2Button is pressed, it will perform an if statement
+     * if the game scene that is paused is the single player scene, pressing the button will resume the single player scene
+     * however, if the player is in the multiplayer scene, it will resume the multiplayer scene
+     */
     return2Button.on("pointerdown", () => {
         if (game.scene.isPaused('Single')) {
             ResumePausedScene('Single');
@@ -20,9 +51,13 @@ pauseScene.create = function () {
             ResumePausedScene('Multi');
         }
     });
-
-    return2Menu = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2, 'singleplayer_button').setDisplaySize(600, 160).setDepth(1).setScale(0.3);
-    return2Menu.setInteractive();
+    
+    /**
+     * return2Menu.on
+     * When return2Menu is pressed, it will perform an if statement
+     * if the game scene that is paused is the single player scene, pressing the button will clear the singleplayer scene and start the QuitToMenu function
+     * however, if the player is in the multiplayer scene, pressing the button will start the QuitToMenu function
+     */    
     return2Menu.on("pointerdown", () => {
         if (game.scene.isPaused('Single')) {
             clearTimeout(singleScene.interval);
@@ -34,16 +69,28 @@ pauseScene.create = function () {
     });
 }
 
+/**
+ * Update()
+ * Updates the scene on every loaded frame. 
+ */
 pauseScene.update = function (time, delta) {
 
 }
 
+/**
+ * QuitToMenu()
+ * Function for changing the scene back to the Main Menu 
+ */
 function QuitToMenu(key) {
     game.scene.stop(key);
     game.scene.start('Menu');
     game.scene.stop('Pause');
 }
 
+/**
+ * ResumePausedScene()
+ * Resumes the scene that is currently being paused  
+ */
 function ResumePausedScene(key) {
     game.scene.run(key);
     game.scene.stop('Pause');
